469f0aec889a6024849ba4ca34ad5ffb39bb948e
cccaelum <ccc.trad@gmail.com>
2024-09-25 16:29:29 +0200

    'finished'

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const { dbConnection} = require('./config/config');
-
+const { dbConnection } = require('./config/config');
 const routes = require('./routes/posts.js');
 
 app.use(express.json());
-
 app.use('/', routes);
 
-
+// Iniciar la conexión a la base de datos
 dbConnection();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Levantar el servidor solo si no estamos en el entorno de pruebas
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}
 
+// Exportar la app para usarla en los tests
 module.exports = app;
--- post.test.js
+++ post.test.js
@@ -1,28 +1,27 @@
-const routes = require('./routes/posts.js');
-const app = require('./app'); 
-const request = require('supertest');
+const app = require('./index.js'); 
+const request = require('supertest'); //para testing de solicitudes HTTP
 
 
 describe('POST /create', () => {
     it('should create a new post when valid data is provided', async () => {
-        const res = await request(app)
-            .post('/create')
-            .json({
+        const res = await request(app) // simulamos una solicitud HTTP a la app
+            .post('/create') 
+            .send({
                 title: 'My First Post',
                 content: 'This is the content of my first post.'
             });
-        expect(res.statusCode).toEqual(201);
-        expect(res.body).toHaveProperty('_id');
-        expect(res.body.title).toEqual('My First Post');
+        expect(res.statusCode).toEqual(201); // Verificamos que el código de estado sea 201 (creado)
+        expect(res.body).toHaveProperty('_id'); // Verificamos que el cuerpo de la respuesta tenga la propiedad _id (indicador de que el post fue creado).
+        expect(res.body.title).toEqual('My First Post'); // Verificamos que el título del post en la respuesta coincida con el enviado
     });
 
     it('should return 400 if title is missing', async () => {
         const res = await request(app)
             .post('/create')
-            .json({
+            .send({
                 content: 'This is the content of my post without a title.'
             });
-        expect(res.statusCode).toEqual(400);
+        expect(res.statusCode).toEqual(400); // Verificamos que el código de estado sea 400 (error del cliente)
         expect(res.body.message).toEqual('Title is required');
     });
 });
